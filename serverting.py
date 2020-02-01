e707aaf6db5f91e3ab9167d64b73b0b6b0e00e55
Tcoss7 <traviscossarini@gmail.com>
2020-02-01 12:46:39 -0500

    Serverting indent fix

--- serverting.py
+++ serverting.py
@@ -1,16 +1,17 @@
 from flask import request, jsonify, render_template
 import sys
 
+
 @app.route("/receiver", methods=["GET", "POST"])
 def my_function():
     if request.method == "POST":
         data = request.get_json()
-		result = ''
-		for item in data:
-			result += str(item['url'])
-			
-		print(result)
-		
+        result = ''
+        for item in data:
+            result += str(item['url'])
+
+        print(result)
+
         return result
     else:
         return render_template('popup.html')
