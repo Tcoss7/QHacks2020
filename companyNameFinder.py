60ddb4f7a126d8276f1f1356bac1f0714f7b7867
Adam Farley <adamfrly@gmail.com>
2020-02-01 11:46:09 -0500

    Combining ethics score finder and company name finder

--- companyNameFinder.py
+++ companyNameFinder.py
@@ -2,22 +2,27 @@ from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 import requests
 
-url = 'https://www.amazon.ca/dp/B07L3R93F2?aaxitk=xL8buRWZjNlIiQISzOBT2A&pd_rd_i=B07L3R93F2&pf_rd_p=aa0a7a36-9d2d-49bd-b59d-ceef667a2a55&hsa_cr_id=6969753180801&sb-ci-n=asinImage&sb-ci-v=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FI%2F81lQp%2BbmaSL.jpg&sb-ci-a=B07L3R93F2'
+url1 = 'https://www.amazon.ca/dp/B07L3R93F2?aaxitk=xL8buRWZjNlIiQISzOBT2A&pd_rd_i=B07L3R93F2&pf_rd_p=aa0a7a36-9d2d-49bd-b59d-ceef667a2a55&hsa_cr_id=6969753180801&sb-ci-n=asinImage&sb-ci-v=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FI%2F81lQp%2BbmaSL.jpg&sb-ci-a=B07L3R93F2'
 
-source = requests.get(url).text
+def find_company(url):
+	source = requests.get(url).text
 
 
-soup = BeautifulSoup(source, 'lxml')
-#keyword bylineinfo
-try:
-	compName = soup.find(id="bylineInfo").text
-	print(soup.prettify)
-except:
-	print("exception code")
-	session = HTMLSession()
-	resp = session.get(url)
-	resp.html.render()
-	source = resp.html.html
 	soup = BeautifulSoup(source, 'lxml')
-	compName = soup.find(id="bylineInfo").text
-	print(compName)
+	#keyword bylineinfo
+	try:
+		compName = soup.find(id="bylineInfo").text
+		print(soup.prettify)
+	except:
+		print("exception code")
+		session = HTMLSession()
+		resp = session.get(url)
+		resp.html.render()
+		source = resp.html.html
+		soup = BeautifulSoup(source, 'lxml')
+		compName = soup.find(id="bylineInfo").text
+		print(compName)
+	return compName
+
+
+find_company(url1)
--- findEthicsScore.py
+++ findEthicsScore.py
@@ -1,14 +1,36 @@
 import mechanize
 from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 import re
+import requests
 
-url = "http://www.corporatecritic.org/companies.aspx"
-companyName = "amazon"
+corporate_critic_url = "http://www.corporatecritic.org/companies.aspx"
+product_url = 'https://www.amazon.ca/dp/B07L3R93F2?aaxitk=xL8buRWZjNlIiQISzOBT2A&pd_rd_i=B07L3R93F2&pf_rd_p=aa0a7a36-9d2d-49bd-b59d-ceef667a2a55&hsa_cr_id=6969753180801&sb-ci-n=asinImage&sb-ci-v=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FI%2F81lQp%2BbmaSL.jpg&sb-ci-a=B07L3R93F2'
 
 
-def findEthicsScore(companyName):
+def find_company(url):
+    source = requests.get(url).text
+
+    soup = BeautifulSoup(source, 'lxml')
+    # keyword bylineinfo
+    try:
+        compName = soup.find(id="bylineInfo").text
+        print(soup.prettify)
+    except:
+        #print("exception code")
+        session = HTMLSession()
+        resp = session.get(url)
+        resp.html.render()
+        source = resp.html.html
+        soup = BeautifulSoup(source, 'lxml')
+        compName = soup.find(id="bylineInfo").text
+        print(compName)
+    return compName
+
+
+def corporate_critic(companyName):
     br = mechanize.Browser()
-    br.open(url)
+    br.open(corporate_critic_url)
     br.select_form('Form1')
     br['txtSearch'] = companyName
     br.submit()
@@ -20,12 +42,12 @@ def findEthicsScore(companyName):
         ethicsScore = re.sub(chr(10), '', ethicsScore)
         ethicsScore = re.sub('\)', '', ethicsScore)
         ethicsScore = re.sub('\(', '', ethicsScore)
-        #print(ethicsScore)
+        print(ethicsScore)
     except:
         ethicsScore = -1
-        #print(ethicsScore)
 
     return ethicsScore
 
 
-findEthicsScore(companyName)
+final_ethics_score = corporate_critic(find_company(product_url))
+print(final_ethics_score)
