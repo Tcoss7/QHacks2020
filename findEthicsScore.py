dd06eb3ac8dd04df4eb37b3c4e23b816bf01c0e7
Adam Farley <adamfrly@gmail.com>
2020-02-01 10:59:57 -0500

    Error handling and output clean up

--- findEthicsScore.py
+++ findEthicsScore.py
@@ -1,8 +1,9 @@
 import mechanize
 from bs4 import BeautifulSoup
+import re
 
 url = "http://www.corporatecritic.org/companies.aspx"
-companyName = "nike"
+companyName = "amazon"
 
 
 def findEthicsScore(companyName):
@@ -11,13 +12,20 @@ def findEthicsScore(companyName):
     br.select_form('Form1')
     br['txtSearch'] = companyName
     br.submit()
-    soup = BeautifulSoup(br.response().read(), 'html.parser')
-    ethicsScore = soup.select_one(".resultlistethiscore").text
-    ethicsScore.strip('\n')
-    ethicsScore.strip(' ')
-    ethicsScore.strip('\t')
-    print(ethicsScore)
+    try:
+        soup = BeautifulSoup(br.response().read(), 'html.parser')
+        ethicsScore = soup.select_one(".resultlistethiscore").text
+        ethicsScore = re.sub(' ', '', ethicsScore)
+        ethicsScore = re.sub(chr(13), '', ethicsScore)
+        ethicsScore = re.sub(chr(10), '', ethicsScore)
+        ethicsScore = re.sub('\)', '', ethicsScore)
+        ethicsScore = re.sub('\(', '', ethicsScore)
+        #print(ethicsScore)
+    except:
+        ethicsScore = -1
+        #print(ethicsScore)
 
     return ethicsScore
+
 
 findEthicsScore(companyName)
