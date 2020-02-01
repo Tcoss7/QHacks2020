e20aebc59ef6bf7b46a1656ff4882256f05dc525
Tcoss7 <traviscossarini@gmail.com>
2020-02-01 10:24:16 -0500

    Slight changes

--- findEthicsScore.py
+++ findEthicsScore.py
@@ -4,13 +4,20 @@ from bs4 import BeautifulSoup
 url = "http://www.corporatecritic.org/companies.aspx"
 companyName = "nike"
 
-br = mechanize.Browser()
-br.open(url)
-br.select_form('Form1')
-br['txtSearch'] = companyName
-br.submit()
-soup = BeautifulSoup(br.response().read(), 'html.parser')
-ethicsScore = soup.select_one(".resultlistethiscore").text
 
-print(ethicsScore)
+def findEthicsScore(companyName):
+    br = mechanize.Browser()
+    br.open(url)
+    br.select_form('Form1')
+    br['txtSearch'] = companyName
+    br.submit()
+    soup = BeautifulSoup(br.response().read(), 'html.parser')
+    ethicsScore = soup.select_one(".resultlistethiscore").text
+    ethicsScore.strip('\n')
+    ethicsScore.strip(' ')
+    ethicsScore.strip('\t')
+    print(ethicsScore)
 
+    return ethicsScore
+
+findEthicsScore(companyName)
--- pollWebsite.py
+++ pollWebsite.py
@@ -1,23 +1,27 @@
-import time
-
-from bs4 import BeautifulSoup as bs
-import urllib.request
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
-searchTerm = "Microsoft"
-url = 'http://www.corporatecritic.org/home.aspx'
-
-driver = webdriver.Chrome('./chromedriver')
-driver.get(url)
-
-searchBox = driver.find_element_by_id("txtSearch")
-searchBox.send_keys(searchTerm)
-
-submit = driver.find_element_by_id("btnSearch")
-submit.click()
-
-ethiScoreElement = driver.find_element_by_class_name("resultlistethiscore")
-print(ethiScoreElement)
+searchTerm = "Nike"
 
 
+
+def pollCorpCritic(searchTerm):
+    url = 'http://www.corporatecritic.org/home.aspx'
+
+    driver = webdriver.Chrome('./chromedriver')
+    driver.get(url)
+
+    searchBox = driver.find_element_by_id("txtSearch")
+    searchBox.send_keys(searchTerm)
+
+    submit = driver.find_element_by_id("btnSearch")
+    submit.click()
+
+    try:
+        ethiScoreElement = driver.find_element_by_class_name("resultlistethiscore").text
+        return ethiScoreElement
+    except:
+        return -1
+
+    driver.close()
+
+pollCorpCritic(searchTerm)
